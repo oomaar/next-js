a24c04fb76fddf3c9c38d1b1736336d86515b253
Omar <omaar_5@hotmail.com>
2021-08-16 21:23:21 +0200

    add: next Image auto lazy load

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
     return (
         <nav>
             <div className="logo">
-                <h1>Ninja List</h1>
+                <Image
+                    src="/logo.jpg"
+                    width={128}
+                    height={77}
+                    alt=""
+                />
             </div>
             <Link href="/"><a>Home</a></Link>
             <Link href="/about"><a>About</a></Link>
