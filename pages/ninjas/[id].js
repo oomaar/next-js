5da4a5e67982ee0e22a01c3b6d662f620848c41d
Omar <omaar_5@hotmail.com>
2021-08-16 22:08:10 +0200

    feat: Dynamic-3: getStaticPaths

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,7 +1,36 @@
-const Ninja = () => {
+export const getStaticPaths = async () => {
+    const res = await fetch(`https://fakestoreapi.com/products`);
+    const data = await res.json();
+
+    const paths = data.map(ninja => {
+        return {
+            params: { id: ninja.id.toString() }
+        };
+    });
+
+    return {
+        paths,
+        fallback: false
+    };
+};
+
+export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+    const data = await res.json();
+
+    return {
+        props: { ninja: data }
+    };
+};
+
+const Ninja = ({ ninja }) => {
     return (
         <div>
-            <h1>Details Page</h1>
+            <h1>{ninja.title}</h1>
+            <p>{ninja.price}</p>
+            <p>{ninja.description}</p>
+            <p>{ninja.category}</p>
         </div>
     );
 };
--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -3,7 +3,7 @@ import HeadTag from "../../global/HeadTag";
 import styles from "../../styles/Ninjas.module.css";
 
 export const getStaticProps = async () => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const res = await fetch(`https://fakestoreapi.com/products`);
     const data = await res.json();
 
     return {
@@ -21,7 +21,7 @@ const Ninjas = ({ ninjas }) => {
             {ninjas.map(ninja => (
                 <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
                     <a className={styles.single}>
-                        <h3>{ninja.name}</h3>
+                        <h3>{ninja.title}</h3>
                     </a>
                 </Link>
             ))}
