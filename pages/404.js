2dd2f137ad988292f3494526f73f51a94875acc7
Omar <omaar_5@hotmail.com>
2021-08-16 21:18:03 +0200

    feat: Auto Redirect

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,16 @@
+import { useRouter } from 'next/dist/client/router';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
 const NotFound = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000);
+    }, []);
+
     return (
         <div className="not-found">
             <h1>Oooooops....</h1>
