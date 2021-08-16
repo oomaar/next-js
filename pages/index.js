789e95c36877e63343fbcf133f848ba68f032ad9
Omar <omaar_5@hotmail.com>
2021-08-16 20:58:20 +0200

    setup: global layout (Master Page)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
@@ -12,13 +10,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar />
       <h1>HomePage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, vel saepe quaerat laudantium numquam reprehenderit explicabo! Qui doloremque quasi voluptate?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, vel saepe quaerat laudantium numquam reprehenderit explicabo! Qui doloremque quasi voluptate?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, vel saepe quaerat laudantium numquam reprehenderit explicabo! Qui doloremque quasi voluptate?</p>
       <Link href="/ninjas"><a>See Ninja Listing</a></Link>
-      <Footer />
     </div>
   )
 }
